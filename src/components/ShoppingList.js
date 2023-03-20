42f62c3194b4b743ec729b5faeb2cb31277537f6
Bill Brown <bill.a.brown90@gmail.com>
2023-03-20 14:05:43 -0700

    learning data fetching in react

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +6,40 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  //!1st step: Call a fetch request for to get data into items state
+  useEffect(()=>{
+    fetch("http://localhost:4000/items")
+    .then(resp=>resp.json())
+    .then(data=>{
+      setItems(data)
+    })
+  }, []);
+  //!End of 1st step
+  //*Adding a new Item to the list
+  //create a callback function to get data from itemForm
+  function handleAddItem(newItem) {
+    console.log(newItem)
+    setItems([...items,newItem]);
+  }
+  //*End of Adding new Item to the list
+  //!Create a callback function to take new item from item remve from cart btn in Item.js
+  function handleUpdateItem(updatedItem){
+    const updatedItems = items.map((item)=>{
+      if(item.id===updatedItem.id){
+        return updatedItem;
+      }else{
+        return item;
+      }
+    })
+    setItems(updatedItems)
+  }
+  //!end of this one
+  //* Create a Callback function for the Delete request button
+  function handleDeleteItem(deletedItem){
+    const updatedItems = items.filter((item)=>item.id !== deletedItem.id);
+    setItems(updatedItems)
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,14 +53,14 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} onDeleteItem={handleDeleteItem}/>
         ))}
       </ul>
     </div>
